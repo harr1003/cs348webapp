a8b20bffd4516e1a36998c1cd9d3ba8547ad7e8a
Natalie <harr1003@purdue.edu>
2025-05-05 12:44:49 -0400

    update signup port

--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -5,19 +5,17 @@ export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const signup = async (username, email, password) => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch(
-      `${import.meta.env.VITE_API_URL}/api/user/signup`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, email, password }),
-      }
-    );
+    const response = await fetch(`${API_URL}/api/user/signup`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, email, password }),
+    });
     const json = await response.json();
 
     if (!response.ok) {
