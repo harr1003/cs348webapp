c8db26819f16a9affdbbea6dd488a1a44d38d65f
Natalie <harr1003@purdue.edu>
2025-05-05 12:01:50 -0400

    Configuration update for env files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  const backendURL = env.VITE_API_URL || "http://localhost:5000";
+  const backendURL = env.VITE_API_URL;
 
   return {
     plugins: [react()],
